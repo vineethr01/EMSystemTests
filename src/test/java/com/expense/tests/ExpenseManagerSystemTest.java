aede77fa345a3b9e81fd26e89008b76771b277b4
vineethr01 <52404513+vineethr01@users.noreply.github.com>
2019-07-04 11:25:09 +0530

    Update ExpenseManagerSystemTest.java

--- src/test/java/com/expense/tests/ExpenseManagerSystemTest.java
+++ src/test/java/com/expense/tests/ExpenseManagerSystemTest.java
@@ -15,8 +15,9 @@ public class ExpenseManagerSystemTest {
 	public void titleTest1() throws Exception{
 		WebDriver driver;
 		driver = new HtmlUnitDriver();
-		Thread.sleep(10000);
+		//Thread.sleep(10000);
 		driver.get("http://localhost:8089/ExpenseApp-1/login.jsp");	
+		Thread.sleep(600000);
 
 		//Comment below line to make test pass
 		//String expectedTitle = "Hello Page";
